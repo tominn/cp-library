b3c2b4ab282193eb128c54ed37a91123703e22e8
tominn <tominn33@gmail.com>
2025-09-05 22:39:38 +0900

    Add trie

--- data_structure/trie.cpp
+++ data_structure/trie.cpp
@@ -1,21 +1,30 @@
 struct Trie {
-  struct Node {
-    map<char,int> to;
-    Node() {
-    }
-  };
-  vector<Node> d;
-  Trie() {
-      d.push_back(Node());
-  }
-  void insert(const string& s) {
-    int v = 0;
-    for (char c : s) {
-      if (!d[v].to.count(c)) {
-        d[v].to[c] = (int)d.size();
+    struct Node {
+        map<char,int> to;
+        Node() {
+        }
+    };
+    vector<Node> d;
+    Trie() {
         d.push_back(Node());
-      }
-      v = d[v].to[c];
     }
-  }
+    void insert(const string& s) {
+        int v = 0;
+        for (char c : s) {
+            if (!d[v].to.count(c)) {
+                d[v].to[c] = (int)d.size();
+                d.push_back(Node());
+            }
+            v = d[v].to[c];
+        }
+        return;
+    }
+    bool find(const string& s) {
+        int v = 0;
+        for (int i = 0; i < (int)s.size(); i++) {
+            if (d[v].to.count(s[i]) == 0) return false;
+            v = d[v].to[s[i]];
+        }
+        return true;
+    }
 };
