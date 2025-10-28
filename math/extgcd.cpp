eb5889c91d878976bb0af69dc84d2cc7b4437697
tominn <tominn33@gmail.com>
2025-10-28 23:09:48 +0900

    add extgcd

--- math/extgcd.cpp
+++ math/extgcd.cpp
@@ -1,9 +1,10 @@
-ll ExtGcd(ll a, ll b, ll& x, ll& y) {
+// ax + by = gcd(a, b)
+ll extgcd(ll a, ll b, ll& x, ll& y) {
     if (b == 0) {
         x = 1, y = 0;
         return a;
     }
-    ll g = ExtGcd(b, a % b, y, x);
+    ll g = extgcd(b, a % b, y, x);
     y -= (a / b) * x;
     return g;
 }
